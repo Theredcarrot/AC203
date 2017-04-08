3e33dcef787a867d569df19a0497b556bb01dc65
theredcarrot <leoc@student.cis.edu.hk>
2017-04-08 12:36:16 +0800

    Lesson2

--- Lesson2/app.js
+++ Lesson2/app.js
@@ -1,1 +1,8 @@
-console.log("Testing")
+console.log("Glad you found me!")
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.fillStyle= "rgb(255,99,71)";
+
+ctx.fillRect(0,0,600,10);
