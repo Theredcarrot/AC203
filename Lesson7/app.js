91f2d1dcd4421d7a81355e3b5fa03a1756681d0a
Leo <leoc@student.cis.edu.hk>
2017-06-03 11:29:17 +0800

    Update app.js

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -95,10 +95,10 @@ function update(){
 	player.body.velocity.x = 0;
 
 	if(cursors.left.isDown){
-		player.body.velocity.x = -150;
+		player.body.velocity.x = -300;
 		player.animations.play('left');
 	}else if(cursors.right.isDown){
-		player.body.velocity.x = 150;
+		player.body.velocity.x = 300;
 		player.animations.play('right');
 	} else {
 		player.animations.stop();
@@ -106,7 +106,7 @@ function update(){
 	}
 
 	if(cursors.up.isDown && player.body.touching.down){
-		player.body.velocity.y = -500;
+		player.body.velocity.y = -400;
 	}
 	
 	if(cursors.down.isDown){
